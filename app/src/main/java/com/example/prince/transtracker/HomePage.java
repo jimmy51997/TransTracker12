c616b2c773bec5178f366f97ad63016b1fa172e8
princeAbhishek <abhishek.bitpatna@gmail.com>
2017-09-10 16:53:56 +0530

    Home page

---
+++ app/src/main/java/com/example/prince/transtracker/HomePage.java
@@ -0,0 +1,17 @@
+package com.example.prince.transtracker;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+    }
+}
--- app/src/main/java/com/example/prince/transtracker/SignupPage.java
+++ app/src/main/java/com/example/prince/transtracker/SignupPage.java
@@ -1,13 +1,26 @@
 package com.example.prince.transtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 public class SignupPage extends AppCompatActivity {
-
+    AppCompatButton singuptohome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+
+        singuptohome = (AppCompatButton)findViewById(R.id.singuptohome);
+
+        singuptohome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(SignupPage.this,HomePage.class);
+                startActivity(i);
+            }
+        });
     }
 }
